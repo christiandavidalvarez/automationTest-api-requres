862bf6f4b5e452776bba75c9514a7197994894b5
christiandavidalvarez <christiandavidalvarez@gmail.com>
2024-03-21 05:54:00 -0500

    create schema to step definitions

---
+++ app/src/test/java/automationtest/api/requres/stepDefinitions/DeletionUserStepDefinitions.java
@@ -0,0 +1,33 @@
+package automationtest.api.requres.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DeletionUserStepDefinitions {
+
+    @Given("Mario have the ID of a user I wish to delete")
+    public void mario_have_the_id_of_a_user_i_wish_to_delete() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("he request the deletion of this user")
+    public void he_request_the_deletion_of_this_user() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should confirm the deletion")
+    public void the_system_should_confirm_the_deletion() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should no longer exist in the system")
+    public void the_user_should_no_longer_exist_in_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ app/src/test/java/automationtest/api/requres/stepDefinitions/ListUserStepDefintions.java
@@ -0,0 +1,33 @@
+package automationtest.api.requres.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ListUserStepDefintions {
+
+    @Given("there are registered users on the platform")
+    public void there_are_registered_users_on_the_platform() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("he request the list of users from a specific page")
+    public void he_request_the_list_of_users_from_a_specific_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("he should receive a list containing the users of that page")
+    public void he_should_receive_a_list_containing_the_users_of_that_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the current page information matches the requested one")
+    public void the_current_page_information_matches_the_requested_one() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
